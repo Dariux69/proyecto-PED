47da0231c3f9fb2aee436eacea35d71399dafc36
Dariux69 <00023525@uca.edu.sv>
2026-02-12 15:59:27 -0600

    Add: funcion mostrar

--- proyecto.cpp
+++ proyecto.cpp
@@ -16,13 +16,14 @@ private:
 public:
     Juego();
     void insertar(void);
+    void mostrarLista(void);
     void menu(void);
 };
 
 Juego::Juego(){
     pInicio = NULL;
 };
-    void insertar;
+
     void Juego::insertar(void){
         Jugador* nuevo;
         string nombre;
@@ -45,11 +46,55 @@ Juego::Juego(){
                 pInicio->ant = nuevo;
             pInicio = nuevo;
         }
-
         } while (respuesta == 's');
     };
-int main()
-{
 
+   void  Juego :: mostrarLista(void){
+    Jugador *s1 = pInicio;
+    Jugador *s2 = NULL;
+    while(s1 != NULL){
+        cout << s1->nombre << "  " <<s1->identificador<< " ";
+        s2 = s1;
+        s1 = s1->sig;
+    }
+    cout << endl;
+    while(s2 != NULL){
+        cout << s2->nombre << "  " << s2->identificador<< " ";
+        s2 = s2->ant;
+    }
+    cout << endl;
+}
+
+void Juego::menu(void){
+    cout<<"endl";
+    cout<<"Las opciones del menu son:"<<endl;
+
+    int opcion;
+    do{
+        cout<< "1) Insertar jugador." << endl
+            << "2) Mostrar la lista." << endl
+            << "3) salir"<<endl;
+            cin>> opcion;
+    switch(opcion){
+        case 1:
+            insertar();
+        break;
+            mostrarLista();
+        case 2:
+        break; 
+
+    };
+    }while(opcion !=3);
+}
+
+
+int main(void){
+    cout<<"endl";
+    cout<< " Prueba de codigo :D"<<endl<<endl;
+
+    Juego miJuego;
+    miJuego.menu();
+
+    cout <<endl;
     return 0;
 }
