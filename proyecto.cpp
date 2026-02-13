d6d76bd9f0db61d903fb7d5f10971a5b2555b742
Bran-ssj <00109425@uca.edu.sv>
2026-02-13 14:34:18 -0600

    fix: funcion insertar con lista circular

--- proyecto.cpp
+++ proyecto.cpp
@@ -9,47 +9,21 @@ struct Jugador{
     Jugador* sig;
     Jugador* ant;
 };
+
 class Juego{
 private:
     Jugador *pInicio;
 
 public:
-    Juego();
-    void insertar(void);
-    void menu(void);
+    Juego(){
+        pInicio = NULL;
+    }
+
+    #include "inscribir.h"
 };
 
-Juego::Juego(){
-    pInicio = NULL;
-};
-    void insertar;
-    void Juego::insertar(void){
-        Jugador* nuevo;
-        string nombre;
-        char respuesta;
-        int identificador;
-        do {
-        cout << "¿Quieres inscribir un jugador nuevo? (s/n)"<< endl;
-        cin >> respuesta;
-        if(respuesta == 's'){
-            cout << "Digite el nombre: "<< endl;
-            cin >> nombre;
-            cout << "Digite un identificador numerico : "<< endl;
-            cin >> identificador;
-            nuevo = new Jugador;
-            nuevo->identificador = identificador;
-            nuevo->nombre = nombre; 
-            nuevo->sig = pInicio; 
-            nuevo->ant = NULL;
-            if(pInicio != NULL)
-                pInicio->ant = nuevo;
-            pInicio = nuevo;
-        }
-
-        } while (respuesta == 's');
-    };
-int main()
-{
-
+int main(){
+    Juego juego;
+    juego.insertar();
     return 0;
 }
